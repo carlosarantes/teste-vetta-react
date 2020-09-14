cc07f2b98f4b33ef9a8c279496849703096da20d
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-09-14 19:28:37 -0300

    teste

--- src/pages/clientes/lista-clientes/Lista.js
+++ src/pages/clientes/lista-clientes/Lista.js
@@ -2,13 +2,60 @@ import React from 'react';
 import './Clientes.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Button } from 'react-bootstrap';
+import { Button, Table, Card } from 'react-bootstrap';
 
 function Lista() {
   return (
     <div className="clientesContainer">
-
-
+        <Card>
+            <Card.Header>
+                Clientes  
+                <Button className="ml-3">Adicionar Novo Cliente</Button>
+            </Card.Header>
+        <Card.Body>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Nome do Cliente</th>
+                        <th>CPF/CPNJ</th>
+                        <th>Tipo Pessoa</th>
+                        <th>Telefones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Juão Paulo</td>
+                            <td>125.914.822-00</td>
+                            <td>F</td>
+                            <td>(34) 99999-9999 / (34) 22222-88888</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Juão Paulo</td>
+                            <td>125.914.822-00</td>
+                            <td>F</td>
+                            <td>(34) 99999-9999 / (34) 22222-88888</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Juão Paulo</td>
+                            <td>125.914.822-00</td>
+                            <td>F</td>
+                            <td>(34) 99999-9999 / (34) 22222-88888</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Juão Paulo</td>
+                            <td>125.914.822-00</td>
+                            <td>F</td>
+                            <td>(34) 99999-9999 / (34) 22222-88888</td>
+                        </tr>
+                    </tbody>
+                </Table>
+        </Card.Body>
+        </Card>
     </div>);
 }
 
